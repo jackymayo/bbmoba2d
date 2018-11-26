ebd16e32f7fbf45243f378f8933d71a835147e46
Jacky <jackymayonaise@gmail.com>
2018-11-26 09:37:33 -0500

    Added player movement with diagonal movement.
    The player movement is not tile to tile. Given it has to be bomberman
    style, corner tiles are going to be an issue.
    (Might consider making tiles the same shape as the players)
    Diagonal movement is also going to be an issue so might be best to
    remove it.

--- Godot/Scripts/GeneralMovement.gd
+++ Godot/Scripts/GeneralMovement.gd
@@ -1,13 +1,48 @@
-extends Node
+extends KinematicBody2D
 
 # class member variables go here, for example:
 # var a = 2
-# var b = "textvar"
+
+var movementSpeed = 300
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+	
+
+
+func get_input():
+	"""
+	Create a velocity vector to pass into Godot physics functions.
+	e.g. move_and_slide( Vector2() ), move_and_collide( Vector2() )
+	
+	Args:
+		None
+		
+	Returns: 
+		A Vector2 object
+	"""
+
+	var velocity = Vector2()
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1	
+	
+	# Normalize vector components so diagonal is not sonic speederinos.
+	velocity = velocity.normalized() * movementSpeed
+	
+	return velocity
+
+func _physics_process(delta):
+	var vector = get_input()
+	move_and_slide(vector)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.

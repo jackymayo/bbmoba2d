f65841d9bdbf1bc46733593e48e0164b03f63c8e
Jacky <jackymayonaise@gmail.com>
2018-12-22 03:13:45 -0500

    omegalul:

--- Scripts/GameLoad.gd
+++ Scripts/GameLoad.gd
@@ -1,26 +1,24 @@
 extends Node
 
 remote func pre_configure_game():
-	var selfPeerID = get_tree().get_network_unique_id()
+
 	
 	var map = load("res://Scenes/Map.tscn").instance()
 	get_node("/root/Server/Game").add_child(map)
+	var Network = get_parent()
 	
-	# Load player
-	var player = preload("res://Scenes/Player.tscn").instance()
-	player.set_name(str(selfPeerID))
-	# player.set_network_master(selfPeerID) #
-	get_node("/root/Server/Game/Map/Players").add_child(player)
-	var isServer = get_tree().is_network_server()
-
+	Network.create_server("joe")
+	var selfPeerID = get_tree().get_network_unique_id()
+	# If you're the server add your player in
+	if get_tree().is_network_server():
+		var new_player = load("res://Scenes/Player.tscn").instance()
+		$'/root/Server/Game/Map/Players'.add_child(new_player)
 	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring
-	if not isServer:
+	else:
 		rpc_id(1, "done_preconfiguring", selfPeerID)
-
 func _ready():
 	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	var Server = load('res://Scripts/Server.gd').new(get_tree())
+	# Initialization here)
 	pre_configure_game()
 
 #func _process(delta):

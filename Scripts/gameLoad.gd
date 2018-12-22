2c121c1c078545e44eaed2bb8d0eaf44ae5da68e
Jacky <jackymayonaise@gmail.com>
2018-12-22 08:00:15 -0500

    Multiplayer support should work now

--- Scripts/gameLoad.gd
+++ Scripts/gameLoad.gd
@@ -1,19 +1,23 @@
 extends Node
 
-func pre_configure_game():
-
+remote func pre_configure_game():
+	
+	# Load the map
 	var map = load("res://Scenes/Map.tscn").instance()
 	get_node("/root/Server/Game").add_child(map)
 	
+	# Host creates server 
 	Network.create_server("joe")
-	var selfPeerID = get_tree().get_network_unique_id()
-	# If you're the server add your player in
-	if get_tree().is_network_server():
-		var new_player = load("res://Scenes/Player.tscn").instance()
-		$'/root/Server/Game/Map/Players'.add_child(new_player)
+	var selfPeerId = get_tree().get_network_unique_id()
+	var new_player = preload("res://Scenes/Player.tscn").instance()
+	new_player.name = str(selfPeerId)
+	new_player.set_network_master(selfPeerId)
+	$'/root/Server/Game/Map/Players'.add_child(new_player)
+	
 	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring
-	else:
-		rpc_id(1, "done_preconfiguring", selfPeerID)
+	#if not get_tree().is_network_server():
+	#	rpc_id(1, "done_preconfiguring", selfPeerId)
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here)
